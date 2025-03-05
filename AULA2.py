f76a3aa0b129065d567b2736471aca5075f101af
Dhiuly Silva <dhiulysoaresds@gmail.com>
2025-03-05 19:05:19 -0300

    aula 2 exercicio 1

---
+++ AULA2.py
@@ -0,0 +1,20 @@
+# Desenvolva um algoritmo que calcule a soma de dois números.
+
+# comandos
+
+# exemplo: 
+# usuario digita 2
+# usuario digita 3
+# soma = 2 + 3 (processamento)
+#resultado = 5 (saida)
+
+primeiro_numero: int = int(input("Digite um número: ")) # a função input é utilizada para receber dados do usuário, para guardar o dado recebido é necessário atribuir a uma variável
+segundo_numero = int(input("Digite outro número: "))
+
+soma = primeiro_numero + segundo_numero # a variável soma recebe a soma dos números digitados
+
+print("A soma dos números é: ", soma) # a função print é utilizada para exibir dados na tela
+
+
+#"8" + "63" = "863"
+#8 + 63 = 71
